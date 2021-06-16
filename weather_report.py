1a78c64ef02db2453e882d71e4d9f0681a8ca9b3
Nithyasree <nithi0253@gmail.com>
2021-06-16 14:20:55 +0530

    weather data for a specific city

---
+++ weather_report.py
@@ -0,0 +1,34 @@
+import requests
+import json
+
+api = "d56c8f69033661adb7ab6298b3df2444"
+
+city = input("Enter your city: ")
+url = f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={api}&units=metric"
+
+# accessing data using requests get() method
+ans = requests.get(url)
+json_data = json.loads(ans.text)
+
+# whole data returned from the api
+# print(json_data)
+
+# checking whether the request is success or not
+if json_data['cod'] != 200:
+    print(json_data['message'])
+
+else:
+    # getting weather data
+    w = json_data["weather"][0]["description"]
+
+    # getting temperature
+    temp = json_data['main']['temp']
+
+    # Humidity
+    hum = json_data['main']['humidity']
+
+    # speed of wind
+    swind = json_data['wind']['speed']
+
+    print(f"Weather : {w}",f"Temperature: {temp}", f"Humidity: {hum}", f"Wind speed: {swind}", sep="\n")
+
